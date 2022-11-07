053ce26abf173dbb5acc3c23016d2aa0902e02b9
AbdurrahmanMoussa <mous0158@algonquinlive.com>
2022-11-07 12:49:23 -0500

    fixed image file names

--- utils/getProjectInfo.tsx
+++ utils/getProjectInfo.tsx
@@ -4,7 +4,7 @@ const getProjectInfo = [
     name: "Pizza Store App",
     image: "/pizzaapp.png",
     description:
-      "A pizza store app that allows users to order pizza and pay for it via cash on delivery or PayPal. Includes Admin panel to manage each specific order and view previous orders as well as ability to add, delete, and update a product.",
+      "A NextJS pizza store app that allows users to order pizza and pay via cash on delivery or PayPal. Includes Admin panel to manage each specific order and view previous orders as well as ability to add, delete, and update a product.",
 
     link: [
       { name: "Code", url: "https://github.com/AbdurrahmanMoussa/Pizza-Store" },
@@ -16,7 +16,7 @@ const getProjectInfo = [
     name: "NHL Team Info App",
     image: "/nhlstatsapp4.png",
     description:
-      "A web app that allows users to view NHL team stats and player stats. Includes a search bar to search for a specific team and view information about that team. Also includes a search bar to search for a specific player and view information about that team. A user can also see any roster in a specific team's history and see career stats and gamelog for any player.",
+      "A NextJS app that allows users to view NHL team stats and player stats. Includes a search bar to search for a specific team and view information about that team. Also includes a search bar to search for a specific player and view information about that team. A user can also see any roster in a specific team's history and see career stats and gamelog for any player.",
 
     link: [
       {
