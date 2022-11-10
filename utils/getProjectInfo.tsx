0036aa44c10f0ba3f93247fa42472583825c063a
AbdurrahmanMoussa <mous0158@algonquinlive.com>
2022-11-10 16:51:34 -0500

    adjusted image url

--- utils/getProjectInfo.tsx
+++ utils/getProjectInfo.tsx
@@ -14,7 +14,8 @@ const getProjectInfo = [
   {
     id: "2",
     name: "NHL Team Info App",
-    image: "/nhlstatsappmain.png",
+    image:
+      "https://res.cloudinary.com/dydzf1y9j/image/upload/v1668116987/nhlstatsappmain_e3wubu.png",
     description:
       "A NextJS app that allows users to view NHL team stats and player stats. Includes a search bar to search for a specific team and view information about that team. Also includes a search bar to search for a specific player and view information about that team. A user can also see any roster in a specific team's history and see career stats and gamelog for any player.",
 
@@ -30,7 +31,8 @@ const getProjectInfo = [
   {
     id: "3",
     name: "YelpCamp App",
-    image: "/yelpcampMain.png",
+    image:
+      "https://res.cloudinary.com/dydzf1y9j/image/upload/v1668116966/yelpcampMain_xl6qkb.png",
     description:
       "A YelpCamp app that allows users to register and login to create, edit, and delete campgrounds as well as review existing campgrounds.",
 
